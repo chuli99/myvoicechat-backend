86f1b5dfb497d54c7f2a482ca72552b72fb7bb99
chuli99 <juliancastillo0399@gmail.com>
2025-05-11 20:57:53 -0300

    #3 Pydantic schemas for data validation

---
+++ app/schemas/user.py
@@ -0,0 +1,38 @@
+from pydantic import BaseModel, EmailStr
+from typing import Optional
+from datetime import datetime
+
+
+class UserBase(BaseModel):
+    username: str
+    email: EmailStr
+    primary_language: Optional[str] = None
+
+
+class UserCreate(UserBase):
+    password: str
+
+
+class UserUpdate(BaseModel):
+    username: Optional[str] = None
+    email: Optional[EmailStr] = None
+    password: Optional[str] = None
+    primary_language: Optional[str] = None
+    is_active: Optional[bool] = None
+
+
+class UserInDBBase(UserBase):
+    id: int
+    created_at: datetime
+    is_active: bool
+
+    class Config:
+        orm_mode = True
+
+
+class User(UserInDBBase):
+    pass
+
+
+class UserInDB(UserInDBBase):
+    hashed_password: str
