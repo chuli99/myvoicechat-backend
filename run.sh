9aa51e269949980c220fcfffa54bd27f17e41a1a
chuli99 <juliancastillo0399@gmail.com>
2025-05-11 20:59:57 -0300

    feat: Scripts for running the application and checking database

---
+++ run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "Starting MyVoiceChat API..."
+echo "Activating virtual environment..."
+
+if [ -d "venv" ]; then
+    source venv/bin/activate
+fi
+
+echo "Installing dependencies..."
+pip install -r requirements.txt
+
+echo "Running migrations..."
+alembic upgrade head
+
+echo "Starting the server..."
+uvicorn main:app --reload --host=0.0.0.0 --port=8000
